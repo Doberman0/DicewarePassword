6f968c52f8789b97a430e81eb886418d7154ddbc
Doberman0 <DabeerM@hotmail.com>
2019-04-02 11:17:53 +0100

    More cryptographicall secure
    Using system random number generation => more random selection of numbers.

--- dware.py
+++ dware.py
@@ -1,7 +1,7 @@
 '''
 Diceware password generator - created for educational purposes only!
 '''
-import random as r #replace with more random OS module
+from random import SystemRandom #More cryptographically secure random number generation
 from dicewarewords import words 
 
 def get_key():
@@ -9,7 +9,7 @@ def get_key():
 	Returns a 5 digit number as a string.
 	Said string is composed of numbers from 1-6 inclusive, thereby simulating 5 die rolls
 	'''
-	return ''.join([str(r.randint(1,6)) for i in range(5)])
+	return ''.join([str(SystemRandom().randrange(6)+1) for i in range(5)])
 
 def get_password():
 	num_of_words = 5
